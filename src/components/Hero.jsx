c401904978b5639a216dbe97f389237a870966dc
kevocode22 <ocampokevin14@gmail.com>
2022-08-12 10:59:29 -0300

    Hero modified

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,8 +8,9 @@ export default function Hero() {
   return (
 
       <div className="flex flex-col justify-center items-center font-['Francois_One']">
-        <h1 className="line-1 anim-typewriter text-white font">Hi!👋 I´m Kevin Ocampo</h1>
-          <h2 className="line-1 anim-typewriter" style={{fontSize:'6vh', color:'#FFC23C', textShadow:'2px 2px 2px 2px black', marginTop:'3vh'}}>I´m a Full Stack MERN Dev</h2>
+        <h1 className="text-white font">Hi!👋 I´m Kevin Ocampo</h1>
+        <h2>I´m a </h2>
+          <h2 className="line-1 anim-typewriter" style={{fontSize:'6vh', color:'#FFC23C', textShadow:'2px 2px 2px 2px black', marginTop:'3vh'}}>Full Stack MERN Dev</h2>
           <div className="buttons-div"><Buttons/></div>
         
       </div>
