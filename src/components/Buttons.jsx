5f0ed25d7b826f42f578249908586784b0eee689
kevocode22 <ocampokevin14@gmail.com>
2022-08-19 02:46:55 -0300

    changes

---
+++ src/components/Buttons.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import '../buttons.css'
+import Linkedin from '../assets/linkedin.png'
+import Github from '../assets/github.png'
+
+const Buttons = () => {
+
+let linkedin = "https://www.linkedin.com/in/kevinocampo22/"
+let git = "https://github.com/kevocode22"
+
+    return (
+        <>
+        
+         <a href={linkedin}><button href={linkedin} style={{width:'25vh', backgroundColor:'#100F0F', color:'#ffff', display:'flex', justifyContent:'center', marginInline:'4vh'}}>
+    <span style={{'marginRight':7, letterSpacing:1}}>Linkedin</span>
+  <img src={Linkedin} style={{height:'3vh'}} alt="Linkedin"/>
+</button></a>
+
+<a href={git}>
+<button style={{width:'25vh', backgroundColor:'#100F0F', color:'#ffff', display:'flex', justifyContent:'center'}}>
+    <span style={{'marginRight':7, letterSpacing:1}}>GitHub</span>
+  <img src={Github} style={{height:'3vh'}} alt="GitHub"/>
+</button></a>
+
+
+        </>
+
+    )
+}
+
+export default Buttons
---
+++ src/components/Hero.jsx
@@ -0,0 +1,19 @@
+/* This example requires Tailwind CSS v2.0+ */
+import '../index.css'
+import Buttons from './Buttons'
+
+export default function Hero() {
+
+
+  return (
+
+      <div className="flex flex-col justify-center items-center font-['Francois_One']">
+        <h1 className="text-white font">Hi!👋 I´m Kevin Ocampo</h1>
+        <h2>I´m a </h2>
+          <h2 className="line-1 anim-typewriter" style={{fontSize:'6vh', color:'#FFC23C', textShadow:'2px 2px 2px 2px black', marginTop:'3vh'}}>Full Stack MERN Dev</h2>
+          <div className="buttons-div"><Buttons/></div>
+        
+      </div>
+
+  )
+}
